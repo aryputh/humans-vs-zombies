177eb46e999eb1c2bf4495f9a542ee332d73a881
HappyGamer3194 <happygamer3194@gmail.com>
2022-07-18 13:00:31 -0700

    Implemented idle and run animations.

--- Assets/Scripts/PlayerMovement/PlayerController.cs
+++ Assets/Scripts/PlayerMovement/PlayerController.cs
@@ -6,19 +6,96 @@ public class PlayerController : MonoBehaviour
 {
     public CharacterController playerController;
     public float speed;
+    public Animator anim;
+
+    [Tooltip("0 = idle, 1 = run, 2 = jump")]
+    public int state;
+
+    private bool isRunning;
+    private bool isIdle;
+    private bool isJumping;
 
     void Start()
     {
-        
+        isIdle = false;
+        isRunning = false;
+        isJumping = false;
     }
 
     void Update()
     {
+        switch (state)
+        {
+            case 2:
+                print("jump");
+
+                isIdle = false;
+                isRunning = false;
+                isJumping = true;
+
+                anim.SetBool("isJumping", true);
+                anim.SetBool("isIdle", false);
+                anim.SetBool("isRunning", false);
+
+                state = 2;
+
+                break;
+            case 1:
+                print("run");
+
+                isIdle = false;
+                isRunning = true;
+                isJumping = false;
+
+                anim.SetBool("isJumping", false);
+                anim.SetBool("isIdle", false);
+                anim.SetBool("isRunning", true);
+
+                state = 1;
+
+                break;
+            case 0:
+                print("idle");
+
+                isIdle = true;
+                isRunning = false;
+                isJumping = false;
+
+                anim.SetBool("isJumping", false);
+                anim.SetBool("isIdle", true);
+                anim.SetBool("isRunning", false);
+
+                state = 0;
+
+                break;
+            default:
+                print("error");
+
+                isIdle = true;
+                isRunning = false;
+                isJumping = false;
+
+                anim.SetBool("isJumping", false);
+                anim.SetBool("isIdle", true);
+                anim.SetBool("isRunning", false);
+
+                state = 0;
+
+                break;
+        }
+        
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
-        Vector3 move = (transform.right * x) + (transform.forward * z);
-
-        playerController.Move(move * speed * Time.deltaTime);
+        if(x != 0 || z != 0)
+        {
+            state = 1;
+            Vector3 move = (transform.right * x) + (transform.forward * z);
+            playerController.Move(move * speed * Time.deltaTime);
+        }
+        else if(x == 0 && z == 0)
+        {
+            state = 0;
+        }
     }
 }
--- Assets/Scripts/ZombieManager.cs
+++ Assets/Scripts/ZombieManager.cs
@@ -14,6 +14,15 @@ public class ZombieManager : MonoBehaviour
     private GameObject player;
     private float sightDistance;
 
+    [Tooltip("0 = idle, 1 = run, 2 = jump")]
+    public int state;
+
+    public Animator anim;
+
+    private bool isRunning;
+    private bool isIdle;
+    private bool isJumping;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -29,11 +38,75 @@ public class ZombieManager : MonoBehaviour
 
         //Rotates to face the player.
         zombie.transform.LookAt(player.transform.position);
+
+        isIdle = false;
+        isRunning = false;
+        isJumping = false;
     }
 
     // Update is called once per frame
     void Update()
     {
+        switch (state)
+        {
+            case 2:
+                print("jump");
+
+                isIdle = false;
+                isRunning = false;
+                isJumping = true;
+
+                anim.SetBool("isJumping", true);
+                anim.SetBool("isIdle", false);
+                anim.SetBool("isRunning", false);
+
+                state = 2;
+
+                break;
+            case 1:
+                print("run");
+
+                isIdle = false;
+                isRunning = true;
+                isJumping = false;
+
+                anim.SetBool("isJumping", false);
+                anim.SetBool("isIdle", false);
+                anim.SetBool("isRunning", true);
+
+                state = 1;
+
+                break;
+            case 0:
+                print("idle");
+
+                isIdle = true;
+                isRunning = false;
+                isJumping = false;
+
+                anim.SetBool("isJumping", false);
+                anim.SetBool("isIdle", true);
+                anim.SetBool("isRunning", false);
+
+                state = 0;
+
+                break;
+            default:
+                print("error");
+
+                isIdle = true;
+                isRunning = false;
+                isJumping = false;
+
+                anim.SetBool("isJumping", false);
+                anim.SetBool("isIdle", true);
+                anim.SetBool("isRunning", false);
+
+                state = 0;
+
+                break;
+        }
+
         //Detects if the player is within the followDistance, if not, it won't follow the player.
         sightDistance = Vector3.Distance(transform.position, player.transform.position);
         if (followDistance >= sightDistance && CanSeePlayer())
@@ -41,10 +114,12 @@ public class ZombieManager : MonoBehaviour
             zombieNav.speed = movementSpeed;
             zombieNav.SetDestination(player.transform.position);
             zombie.transform.LookAt(player.transform.position);
+            state = 1;
         }
         else
         {
             zombieNav.speed = 0;
+            state = 0;
         }
     }
 
